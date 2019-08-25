7dcc54865f65b5fb6c27f2ac6f3af2c55c4403ce
Ivan Ivanov <wankata@ivanovi.net>
2019-08-25 14:22:01 +0300

    urls.defaults is removed in Django 1.6.

--- src/django_detect_cyclic/urls.py
+++ src/django_detect_cyclic/urls.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('django_detect_cyclic.views',
